68313af5f66e155cc2dfbec14cc278e98b2fdedc
0u812 <0u812@github.com>
2014-08-13 13:07:45 -0700

    initl commit

---
+++ source/platform/Backtrace.cpp
@@ -0,0 +1,15 @@
+/*
+ * Backtrace.cpp
+ *
+ *  Created on: Aug 13, 2014
+ *      Author: JKM
+ */
+
+#include "Backtrace.h"
+
+namespace rr
+{
+
+
+
+}
---
+++ source/platform/Backtrace.h
@@ -0,0 +1,16 @@
+#ifndef rrPlatformH
+#define rrPlatformH
+#include "rrExporter.h"
+
+// backtrace dumping in libstdc++
+#if defined(__GLIBCXX__)
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
+
+namespace rr
+{
+    
+} // namespace rr
+
+#endif
