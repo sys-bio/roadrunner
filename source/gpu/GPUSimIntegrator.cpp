0efc22fada3edb4bf20b000600a307ff973ce413
0u812 <0u812@github.com>
2014-08-13 16:04:18 -0700

    gpusim integrator

---
+++ source/gpu/GPUSimIntegrator.cpp
@@ -0,0 +1,119 @@
+#pragma hdrstop
+#include "GPUSimIntegrator.h"
+#include "GPUSimException.h"
+#include "rrExecutableModel.h"
+#include "rrException.h"
+#include "rrLogger.h"
+#include "rrStringUtils.h"
+#include "rrException.h"
+#include "rrUtils.h"
+
+#include <cvode/cvode.h>
+#include <cvode/cvode_dense.h>
+#include <nvector/nvector_serial.h>
+#include <cstring>
+#include <iomanip>
+#include <math.h>
+#include <map>
+#include <algorithm>
+#include <assert.h>
+#include <Poco/Logger.h>
+
+
+using namespace std;
+namespace rr
+{
+namespace rrgpu
+{
+
+void GPUSimIntegrator::setListener(IntegratorListenerPtr p)
+{
+    throw_gpusim_exception("not supported");
+}
+
+IntegratorListenerPtr GPUSimIntegrator::getListener()
+{
+    throw_gpusim_exception("not supported");
+}
+
+GPUSimIntegrator::GPUSimIntegrator(ExecutableModel *aModel, const SimulateOptions* options)
+{
+    Log(Logger::LOG_INFORMATION) << "creating GPUSimIntegrator";
+}
+
+GPUSimIntegrator::~GPUSimIntegrator()
+{
+}
+
+void GPUSimIntegrator::setSimulateOptions(const SimulateOptions* o)
+{
+}
+
+double GPUSimIntegrator::integrate(double timeStart, double hstep)
+{
+    throw_gpusim_exception("not supported");
+}
+
+
+void GPUSimIntegrator::restart(double time)
+{
+}
+_xmlNode* GPUSimIntegrator::createConfigNode()
+{
+    throw_gpusim_exception("not supported");
+}
+
+void GPUSimIntegrator::loadConfig(const _xmlDoc* doc)
+{
+    throw_gpusim_exception("not supported");
+}
+
+void GPUSimIntegrator::setValue(const std::string& key, const rr::Variant& value)
+{
+    throw_gpusim_exception("not supported");
+}
+
+Variant GPUSimIntegrator::getValue(const std::string& key) const
+{
+    throw_gpusim_exception("not supported");
+}
+
+bool GPUSimIntegrator::hasKey(const std::string& key) const
+{
+    throw_gpusim_exception("not supported");
+}
+
+int GPUSimIntegrator::deleteValue(const std::string& key)
+{
+    throw_gpusim_exception("not supported");
+}
+
+std::vector<std::string> GPUSimIntegrator::getKeys() const
+{
+    throw_gpusim_exception("not supported");
+}
+
+
+std::string GPUSimIntegrator::toString() const
+{
+    std::stringstream ss;
+    ss << "< roadrunner.GPUSimIntegrator() >\n";
+
+    return ss.str();
+}
+
+std::string GPUSimIntegrator::toRepr() const
+{
+    std::stringstream ss;
+    ss << "< roadrunner.GPUSimIntegrator() >\n";
+    return ss.str();
+}
+
+std::string GPUSimIntegrator::getName() const
+{
+    return "gpusim";
+}
+
+} // namespace rrgpu
+
+} // namespace rr
---
+++ source/gpu/GPUSimIntegrator.h
@@ -0,0 +1,110 @@
+#ifndef rrCvodeInterfaceH
+#define rrCvodeInterfaceH
+
+#include "Integrator.h"
+#include "rrRoadRunnerOptions.h"
+#include "Configurable.h"
+
+#include <string>
+#include <vector>
+
+/**
+ * CVode vector struct
+ */
+typedef struct _generic_N_Vector *N_Vector;
+
+namespace rr
+{
+
+using std::string;
+
+class ExecutableModel;
+class RoadRunner;
+
+namespace rrgpu
+{
+
+/**
+ * @internal
+ * The integrator implemented by CVODE.
+ */
+class GPUSimIntegrator : public Integrator
+{
+public:
+    GPUSimIntegrator(ExecutableModel* oModel, const SimulateOptions* options);
+
+    virtual ~GPUSimIntegrator();
+
+    /**
+     * creates a new xml element that represent the current state of this
+     * Configurable object and all if its child objects.
+     */
+    virtual _xmlNode *createConfigNode();
+
+    /**
+     * Given an xml element, the Configurable object should pick its needed
+     * values that are stored in the element and use them to set its
+     * internal configuration state.
+     */
+    virtual void loadConfig(const _xmlDoc* doc);
+
+    virtual double integrate(double t0, double tf);
+
+    /**
+     * copies the state vector out of the model and into the integrator vector,
+     * re-initializes the integrator.
+     */
+    virtual void restart(double timeStart);
+
+    /**
+     * set the options the integrator will use.
+     */
+    virtual void setSimulateOptions(const SimulateOptions* options);
+
+    /**
+     * the integrator can hold a single listener. If clients require multicast,
+     * they can create a multi-cast listener.
+     */
+    virtual void setListener(IntegratorListenerPtr);
+
+    /**
+     * get the integrator listener
+     */
+    virtual IntegratorListenerPtr getListener();
+
+    /**
+     * implement dictionary interface
+     */
+    virtual void setValue(const std::string& key, const rr::Variant& value);
+
+    virtual Variant getValue(const std::string& key) const;
+
+    virtual bool hasKey(const std::string& key) const;
+
+    virtual int deleteValue(const std::string& key);
+
+    virtual std::vector<std::string> getKeys() const;
+
+    /**
+     * get a description of this object, compatable with python __str__
+     */
+    virtual std::string toString() const;
+
+    /**
+     * get a short descriptions of this object, compatable with python __repr__.
+     */
+    virtual std::string toRepr() const;
+
+    /**
+     * get the name of this integrator
+     */
+    virtual std::string getName() const;
+
+private:
+};
+
+} // namespace rrgpu
+
+} // namespace rr
+
+#endif
