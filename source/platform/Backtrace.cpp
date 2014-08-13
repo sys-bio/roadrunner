a013375aa05450d842ee737de24ce93c19fb411b
0u812 <0u812@github.com>
2014-08-13 13:43:24 -0700

    add sections and dumpBacktrace impl

--- source/platform/Backtrace.cpp
+++ source/platform/Backtrace.cpp
@@ -1,3 +1,7 @@
+// == PREAMBLE ================================================
+
+// * Licensed under the Apache License, Version 2.0; see README
+
 /*
  * Backtrace.cpp
  *
@@ -5,11 +9,35 @@
  *      Author: JKM
  */
 
+// == INCLUDES ================================================
+
 #include "Backtrace.h"
+#include "nullptr.h"
+
+// backtrace dumping in libstdc++
+#if defined(__GLIBCXX__)
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
+
+// == CODE ====================================================
 
 namespace rr
 {
 
+void dumpBacktrace(std::ostream& s) {
+    void** addrbuf = nullptr;
+    int max = 32;
+    int size;
+    do {
+        if(addrbuf)
+            free(addrbuf);
 
+        addrbuf = (void**)malloc(max*sizeof(*addrbuf));
+        size = backtrace(addrbuf, max);
+        std::cerr << "size = " << size << ", max = " << max << std::endl;
+
+    } while((size == max) && (max <<= 1));
+}
 
 }
