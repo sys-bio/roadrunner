f73a4ec3944b8265de343c4d9fee5460e537a685
0u812 <0u812@github.com>
2014-08-15 16:21:33 -0700

    added docs

--- source/patterns/Range.hpp
+++ source/patterns/Range.hpp
@@ -25,6 +25,36 @@ namespace rr
  * @author JKM
  * @brief Allows iterating over a range,
  * defined by begin and end iterators
+ * @details Use this class with a custom type that defines
+ * non-standard begin/end functions. The result is an object
+ * with the usual begin/end iterator semantics, which enables
+ * use within a range-based for loop, among other things.
+ * @verbat
+ * // example usage
+ * class graph {
+ *     node_iterator nodes_begin();
+ *     node_iterator nodes_end();\n
+ *     // return a range of all nodes
+ *     Range<graph::node_iterator> nodes() {
+ *         return Range<graph::node_iterator>(g.nodes_begin(), g.nodes_end());
+ *     }\n
+ *     edge_iterator edges_begin();
+ *     edge_iterator edges_end();\n
+ *     // return a range of all edges
+ *     Range<graph::edge_iterator> edges() {
+ *         return Range<graph::edge_iterator>(g.edges_begin(), g.edges_end());
+ *     }\n
+ * };\n
+ * // type graph has no begin/end functions and therefore cannot
+ * // be used in a range-based for, but its range methods can
+ * graph g;\n
+ * // loop over nodes
+ * for (node n : g.nodes())
+ *     std::cout << n << std::endl;\n
+ * // loop over edges
+ * for (edge e : g.edges)
+ *     std::cout << e << std::endl;
+ * @endverbat
  */
 template <class Iterator>
 class Range {
@@ -36,12 +66,12 @@ public:
     Range(IteratorType begin, IteratorType end)
         : begin_(begin), end_(end) {}
 
-    /// Construct from container
+    /// Construct from container with begin/end functions
     template<class Contiainer>
     Range(Contiainer& c)
         : begin_(c.begin()), end_(c.end()) {}
 
-    /// Construct from container
+    /// Construct from container with begin/end functions
     template<class Contiainer>
     Range(const Contiainer& c)
         : begin_(c.begin()), end_(c.end()) {}
