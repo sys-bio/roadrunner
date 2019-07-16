67453fcfcd0073bbc85c133e711142ba3e5c1fb6
O. O. Man <atb8888@comcast.net>
2019-07-16 15:35:51 -0700

    Continue test suite

--- wrappers/C/rrc_api.cpp
+++ wrappers/C/rrc_api.cpp
@@ -626,12 +626,12 @@ RRCDataPtr rrcCallConv simulate(RRHandle handle)
 	if (false)
 	{
 		rri = new RoadRunner();
-		rri = new  RoadRunner("D:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00006/00006-sbml-l2v4-mod.xml");
-		rri->getSimulateOptions().loadSBMLSettings("D:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00006/00006-settings.txt");
+		rri = new  RoadRunner("C:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00010/00010-sbml-l2v4-mod.xml");
+		rri->getSimulateOptions().loadSBMLSettings("C:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00010/00010-settings.txt");
 		rri->simulate();
 		RoadRunnerData resultData(rri);
 
-		ofstream fs("D:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00006/00006-results.csv");
+		ofstream fs("C:/rr/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/00010/00010-results.csv");
 		fs << resultData;
 		fs.close();
 	}
