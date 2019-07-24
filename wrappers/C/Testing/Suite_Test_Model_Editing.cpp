05e06c663a32fe364ae6cf0c573589a613bd57da
andrewbriand <atb8888@comcast.net>
2019-07-24 16:30:37 -0700

    Model Editing C API tests

--- wrappers/C/Testing/Suite_Test_Model_Editing.cpp
+++ wrappers/C/Testing/Suite_Test_Model_Editing.cpp
@@ -26,17 +26,18 @@ extern string gTSModelsPath;
 extern string gCompiler;
 
 /*
-* Loads <prefix>/source/roadrunner/models/sbml-test-suite/cases/semantic/model_editing/NNNNN/NNNNN-sbml-*VERSION*.xml
-* where NNNNN is case number
-* applies modification to the resulting roadrunner instance and compares the result to NNNNN-results.csv in the same folder, 
-* which should be the result of running the model NNNNN-sbml-*VERSION*-mod.xml, which should be the model expected after applying
-* modification to the original model
+* Loads <prefix>/source/roadrunner/models/sbml-test-suite/cases/semantic/<suite-name>/<test-name>/<test-name>-sbml-*VERSION*.xml
+* applies modification to the resulting roadrunner instance and compares the result to <test-name>-results.csv in the same folder,
+* The method obtains test-name and suite-name from UnitTest++ so this method must be run within a UnitTest++ test
 * Returns true if the results are close enough, false otherwise
 */
-bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::string version = "l2v4")
+bool RunTestWithModification(void(*modification)(RRHandle), std::string version = "l2v4")
 {
 	bool result(false);
 	RRHandle gRR;
+
+	string testName(UnitTest::CurrentTest::Details()->testName);
+	string suiteName(UnitTest::CurrentTest::Details()->suiteName);
 
 	//Create instance..
 	gRR = createRRInstanceEx(gTempFolder.c_str(), gCompiler.c_str());
@@ -52,23 +53,21 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 
 	try
 	{
-		Log(Logger::LOG_NOTICE) << "Running Test: " << caseNumber << endl;
-		string dataOutputFolder(gTempFolder + "/model_editing");
+		Log(Logger::LOG_NOTICE) << "Running Test: " << testName << endl;
+		string dataOutputFolder(joinPath(gTempFolder, suiteName));
 		string dummy;
-		string logFileName;
 		string settingsFileName;
 
 		setCurrentIntegratorParameterBoolean(gRR, "stiff", 0);
 
 		//Create a log file name
-		createTestSuiteFileNameParts(caseNumber, ".log", dummy, logFileName, settingsFileName);
 		if (!createFolder(dataOutputFolder))
 		{
 			string msg("Failed creating output folder for data output: " + dataOutputFolder);
 			throw(rr::Exception(msg));
 		}
 		//Create subfolder for data output
-		dataOutputFolder = joinPath(dataOutputFolder, getTestSuiteSubFolderName(caseNumber));
+		dataOutputFolder = joinPath(dataOutputFolder, testName);
 
 		if (!createFolder(dataOutputFolder))
 		{
@@ -81,11 +80,14 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 		simulation.UseHandle(gRR);
 
 		//Read SBML models.....
-		string modelFilePath(getParentFolder(getParentFolder(getParentFolder(gTSModelsPath))) + "/MODEL_EDITING_TEST_SUITE");
+		string modelFilePath(joinPath(getParentFolder(getParentFolder(getParentFolder(gTSModelsPath))), suiteName));
 		string modelFileName;
 
-		simulation.SetCaseNumber(caseNumber);
-		createTestSuiteFileNameParts(caseNumber, "-sbml-" + version + ".xml", modelFilePath, modelFileName, settingsFileName);
+		simulation.SetCaseNumber(0);
+
+		modelFilePath = joinPath(modelFilePath, testName);
+		modelFileName = testName + "-sbml-" + version + ".xml";
+		settingsFileName = testName + "-settings.txt";
 
 		//The following will load and compile and simulate the sbml model in the file
 		simulation.SetModelFilePath(modelFilePath);
@@ -131,12 +133,11 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 		rri->load(fullPath, &opt);
 
 		//Then read settings file if it exists..
-		string settingsOveride("");
-		if (!simulation.LoadSettings(settingsOveride))
+		if (!simulation.LoadSettings(joinPath(modelFilePath, settingsFileName)))
 		{
 			throw(Exception("Failed loading simulation settings"));
 		}
-		modification(rri);
+		modification(gRR);
 		//Then Simulate model
 		if (!simulation.Simulate())
 		{
@@ -150,7 +151,7 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 			throw(Exception("Failed saving result"));
 		}
 
-		if (!simulation.LoadReferenceData())
+		if (!simulation.LoadReferenceData(modelFilePath + "/" + testName + "-results.csv"))
 		{
 			throw(Exception("Failed Loading reference data"));
 		}
@@ -161,7 +162,7 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 		result = simulation.SaveModelAsXML(dataOutputFolder) && result;
 		if (!result)
 		{
-			Log(Logger::LOG_WARNING) << "\t\t =============== Test " << caseNumber << " failed =============\n";
+			Log(Logger::LOG_WARNING) << "\t\t =============== Test " << testName << " failed =============\n";
 		}
 		else
 		{
@@ -171,257 +172,191 @@ bool RunModelEditingTest(int caseNumber, void(*modification)(RoadRunner*), std::
 	catch (std::exception& ex)
 	{
 		string error = ex.what();
-		cerr << "Case " << caseNumber << ": Exception: " << error << endl;
+		cerr << "Case " << testName << ": Exception: " << error << endl;
 		freeRRInstance(gRR);
 		return false;
 	}
 
 	freeRRInstance(gRR);
 	return result;
-}
-
-bool RunTestWithEdit(const string& version, int caseNumber, void(*edit)(RoadRunner*, libsbml::SBMLDocument*), std::string editName)
-{
-	bool result(false);
-	RRHandle gRR;
-
-	//Create instance..
-	gRR = createRRInstanceEx(gTempFolder.c_str(), gCompiler.c_str());
-
-
-	//Setup environment
-	setTempFolder(gRR, gTempFolder.c_str());
-	libsbml::SBMLDocument doc;
-
-	if (!gRR)
-	{
-		return false;
-	}
-
-	try
-	{
-		Log(Logger::LOG_NOTICE) << "Running Test: " << caseNumber << endl;
-		string dataOutputFolder(gTempFolder + "/" + editName);
-		string dummy;
-		string logFileName;
-		string settingsFileName;
-
-		setCurrentIntegratorParameterBoolean(gRR, "stiff", 0);
-
-		//Create a log file name
-		createTestSuiteFileNameParts(caseNumber, ".log", dummy, logFileName, settingsFileName);
-		if (!createFolder(dataOutputFolder))
-		{
-			string msg("Failed creating output folder for data output: " + dataOutputFolder);
-			throw(rr::Exception(msg));
-		}
-		//Create subfolder for data output
-		dataOutputFolder = joinPath(dataOutputFolder, getTestSuiteSubFolderName(caseNumber));
-
-		if (!createFolder(dataOutputFolder))
-		{
-			string msg("Failed creating output folder for data output: " + dataOutputFolder);
-			throw(rr::Exception(msg));
-		}
-
-		SBMLTestSuiteSimulation_CAPI simulation(dataOutputFolder);
-
-		simulation.UseHandle(gRR);
-
-		//Read SBML models.....
-		string modelFilePath(gTSModelsPath);
-		string modelFileName;
-
-		simulation.SetCaseNumber(caseNumber);
-		createTestSuiteFileNameParts(caseNumber, "-sbml-" + version + ".xml", modelFilePath, modelFileName, settingsFileName);
-
-		//The following will load and compile and simulate the sbml model in the file
-		simulation.SetModelFilePath(modelFilePath);
-		simulation.SetModelFileName(modelFileName);
-		simulation.ReCompileIfDllExists(true);
-		simulation.CopyFilesToOutputFolder();
-		setTempFolder(gRR, simulation.GetDataOutputFolder().c_str());
-		setComputeAndAssignConservationLaws(gRR, false);
-
-		libsbml::SBMLReader reader;
-		std::string fullPath = modelFilePath + "/" + modelFileName;
-		doc = *reader.readSBML(fullPath);
-
-		if (!simulation.LoadSBMLFromFile())
-		{
-			throw(Exception("Failed loading sbml from file"));
-		}
-
-
-		//Check first if file exists first
-		if (!fileExists(fullPath))
-		{
-			Log(Logger::LOG_ERROR) << "sbml file " << fullPath << " not found";
-			throw(Exception("No such SBML file: " + fullPath));
-		}
-
-		RoadRunner* rri = (RoadRunner*)gRR;
-
-		LoadSBMLOptions opt;
-
-		// don't generate cache for models
-		opt.modelGeneratorOpt = opt.modelGeneratorOpt | LoadSBMLOptions::RECOMPILE;
-
-		opt.modelGeneratorOpt = opt.modelGeneratorOpt | LoadSBMLOptions::MUTABLE_INITIAL_CONDITIONS;
-
-		opt.modelGeneratorOpt = opt.modelGeneratorOpt & ~LoadSBMLOptions::READ_ONLY;
-
-		opt.modelGeneratorOpt = opt.modelGeneratorOpt | LoadSBMLOptions::OPTIMIZE_CFG_SIMPLIFICATION;
-
-		opt.modelGeneratorOpt = opt.modelGeneratorOpt | LoadSBMLOptions::OPTIMIZE_GVN;
-
-
-		rri->load(fullPath, &opt);
-
-		//Then read settings file if it exists..
-		string settingsOveride("");
-		if (!simulation.LoadSettings(settingsOveride))
-		{
-			throw(Exception("Failed loading simulation settings"));
-		}
-		//Perform the model editing action
-		edit(castToRoadRunner(gRR), &doc);
-		//Then Simulate model
-		if (!simulation.Simulate())
-		{
-			throw(Exception("Failed running simulation"));
-		}
-
-		//Write result
-		if (!simulation.SaveResult())
-		{
-			//Failed to save data
-			throw(Exception("Failed saving result"));
-		}
-
-		if (!simulation.LoadReferenceData())
-		{
-			throw(Exception("Failed Loading reference data"));
-		}
-
-		simulation.CreateErrorData();
-		result = simulation.Pass();
-		result = simulation.SaveAllData() && result;
-		result = simulation.SaveModelAsXML(dataOutputFolder) && result;
-		if (!result)
-		{
-			Log(Logger::LOG_WARNING) << "\t\t =============== Test " << caseNumber << " failed =============\n";
-		}
-		else
-		{
-			Log(Logger::LOG_NOTICE) << "\t\tTest passed.\n";
-		}
-	}
-	catch (std::exception& ex)
-	{
-		string error = ex.what();
-		cerr << "Case " << caseNumber << ": Exception: " << error << endl;
-		freeRRInstance(gRR);
-		return false;
-	}
-
-	freeRRInstance(gRR);
-	return result;
-}
-
-void readdAllReactions(RoadRunner *rri, libsbml::SBMLDocument *doc)
-{
-	libsbml::ListOfReactions *reactionsToAdd = doc->getModel()->getListOfReactions();
-	std::vector<std::string> currReactionIds = rri->getReactionIds();
-	for (int i = 0; i < reactionsToAdd->size(); i++)
-	{
-		libsbml::Reaction *next = reactionsToAdd->get(i);
-		if (std::find(currReactionIds.begin(), currReactionIds.end(), next->getId()) ==
-			currReactionIds.end())
-			rri->addReaction(next->toSBML());
-	}
-}
-
-void readdAllSpecies(RoadRunner *rri, libsbml::SBMLDocument *doc)
-{
-	libsbml::ListOfSpecies *speciesToAdd = doc->getModel()->getListOfSpecies();
-	std::vector<std::string> currSpeciesIds = rri->getBoundarySpeciesIds();
-	for (std::string s : rri->getFloatingSpeciesIds())
-		currSpeciesIds.push_back(s);
-	if (speciesToAdd->size() > 0)
-	{
-		libsbml::Species *next;
-		for (int i = 0; i < speciesToAdd->size(); i++)
-		{
-			next = speciesToAdd->get(i);
-			if (std::find(currSpeciesIds.begin(), currSpeciesIds.end(), next->getId()) == currSpeciesIds.end())
-				rri->addSpecies(next->getId(), next->getCompartment(), next->getInitialConcentration(), "concentration", false);
-		}
-	}
-}
-
-/*
-* Pre: the model must not contain any rules reliant on species values
-*/
-void removeAndReaddAllSpecies(RoadRunner *rri, libsbml::SBMLDocument *doc)
-{
-	//Remove all species
-	std::vector<std::string> floatingSpeciesIds = rri->getFloatingSpeciesIds();
-	for (std::string sid : floatingSpeciesIds)
-	{
-		rri->removeSpecies(sid, false);
-	}
-
-	std::vector<std::string> boundarySpeciesIds = rri->getBoundarySpeciesIds();
-	for (std::string sid : boundarySpeciesIds)
-	{
-		rri->removeSpecies(sid, false);
-	}
-
-	//Readd all species
-	//libsbml::ListOfSpecies *speciesToAdd = doc->getModel()->getListOfSpecies();
-	//if (speciesToAdd->size() > 0)
-	//{
-	//	libsbml::Species *next;
-	//	for (int i = 0; i < speciesToAdd->size(); i++)
-	//	{
-	//		next = speciesToAdd->get(i);
-	//		rri->addSpecies(next->getId(), next->getCompartment(), next->getInitialConcentration(), "concentration", false);
-	//	}
-	//}
-	readdAllSpecies(rri, doc);
-
-	readdAllReactions(rri, doc);
-}
-
-void removeAndReaddAllReactions(RoadRunner *rri, libsbml::SBMLDocument *doc)
-{
-	std::vector<std::string> reactionIds = rri->getReactionIds();
-	for (std::string rid : reactionIds)
-	{
-		rri->removeReaction(rid, false);
-	}
-	readdAllReactions(rri, doc);
-}
-
-void removeAndReaddAllCompartments(RoadRunner *rri, libsbml::SBMLDocument *doc)
-{
-	std::vector<std::string> compartmentIds = rri->getCompartmentIds();
-	for (std::string cid : compartmentIds)
-	{
-		rri->removeCompartment(cid, false);
-	}
-
-	libsbml::ListOfCompartments *compartmentsToAdd = doc->getModel()->getListOfCompartments();
-	for (int i = 0; i < compartmentsToAdd->size(); i++)
-	{
-		libsbml::Compartment * next = compartmentsToAdd->get(i);
-		rri->addCompartment(next->getId(), next->getVolume(), false);
-	}
-	readdAllSpecies(rri, doc);
-	readdAllReactions(rri, doc);
 }
 
 SUITE(MODEL_EDITING_TEST_SUITE)
 {
+	TEST(ADD_REACTION_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri) {
+			const char* reactants[] = {"S2"};
+			const char* products[] = {"S1"};
+			addReaction(rri, "reaction2", reactants, 1, products, 1, "k1*S2");
+		}));
+	}
+
+	TEST(REMOVE_REACTION_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri) {
+			removeReaction(rri, "reaction2");
+		}));
+	}
+
+	TEST(ADD_SPECIES_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addSpecies(rri, "S3", "compartment", 0.0015, "substance");
+		}));
+	}
+
+	TEST(REMOVE_SPECIES_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			removeSpeciesNoRegen(rri, "S2");
+			addSpeciesNoRegen(rri, "S3", "compartment", 0.00030, "substance");
+			const char* reactants1[] = {"S1"};
+			const char* products1[] = {"S3"};
+			addReactionNoRegen(rri, "reaction1", reactants1, 1, products1, 1, "k1*S1");
+			const char* reactants2[] = {"S3"};
+			const char* products2[] = {"S1"};
+			addReaction(rri, "reaction2", reactants2, 1, products2, 1, "k2*S3");
+		}));
+	}
+
+	TEST(ADD_EVENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addEventNoRegen(rri, "event1", true, "S1 > 0.00015");
+			addEventAssignment(rri, "event1", "S1", "1");
+		}));
+	}
+
+	TEST(EVENT_PRIORITY_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addEvent(rri, "_E0", true, "time >= 0.99");
+			addPriority(rri, "_E0", "1");
+			addEventAssignment(rri, "_E0", "S1", "4");
+			addEventAssignment(rri, "_E0", "S2", "5");
+			addEventAssignment(rri, "_E0", "S3", "6");
+
+			addEvent(rri, "_E1", true, "time >= 0.99");
+			addPriority(rri, "_E1", "0");
+			addEventAssignment(rri, "_E1", "S1", "1");
+			addEventAssignment(rri, "_E1", "S2", "2");
+			addEventAssignment(rri, "_E1", "S3", "3");
+		}, "l3v1"));
+	}
+
+	TEST(EVENT_DELAY_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addDelay(rri, "event1", "1");
+		}));
+	}
+
+	TEST(REMOVE_EVENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			removeEvent(rri, "event1");
+		}));
+	}
+	
+	TEST(ADD_PARAMETER_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addParameterNoRegen(rri, "k1", 0.75);
+			const char* reactants[] = { "S1", "S2" };
+			const char* products[] = {"S3"};
+			addReaction(rri, "reaction1", reactants, 2, products, 1, "k1*S1*S2");
+		}));
+	}
+
+	TEST(ADD_COMPARTMENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addCompartment(rri, "compartment", 1);
+			const char* reactants[] = { "S1", "S2" };
+			const char* products[] = {"2S2"};
+			addReaction(rri, "reaction1", reactants, 2, products, 1, "compartment * k1 * S1 * S2");
+		}));
+	}
+
+	TEST(ADD_ASSIGNMENT_RULE_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addAssignmentRule(rri, "S1", "7");
+		}));
+	}
+	
+	TEST(ADD_RATE_RULE_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addRateRule(rri, "S1", "7");
+		}));
+	}
+
+	TEST(SET_KINETIC_LAW_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			setKineticLaw(rri, "reaction2", "compartment * k2 * S3");
+		}));
+	}
+
+	TEST(ADD_EVENT_ASSIGNMENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addEventAssignment(rri, "event2", "S3", "1");
+		}));
+	}
+
+	TEST(ADD_TRIGGER_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			addTrigger(rri, "event1", "S1 < 0.75");
+		}));
+	}
+
+	TEST(PAUSE_10)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			simulate(rri);
+			addDelay(rri, "event1", "0.2");
+		}));
+	}
+
+	TEST(REMOVE_COMPARTMENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			removeCompartment(rri, "compartment");
+		}));
+	}
+
+	TEST(REMOVE_PARAM_RECURSE_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			removeParameter(rri, "k2");
+		}));
+	}
+
+	TEST(REMOVE_EVENT_ASSIGNMENT_1)
+	{
+		CHECK(RunTestWithModification([](RRHandle rri)
+		{
+			removeEventAssignments(rri, "event1", "S2");
+		}));
+	}
 }
