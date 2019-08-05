6be634e8ab56a19169bd978fb9382eb24a0f52a2
Faye Yu <mengfy0718@gmail.com>
2019-08-05 12:22:10 -0700

    Remove unnecessary rules

--- source/rrRoadRunner.cpp
+++ source/rrRoadRunner.cpp
@@ -5730,9 +5730,9 @@ void RoadRunner::addAssignmentRule(const std::string& vid, const std::string& fo
 		throw std::invalid_argument("Roadrunner::addAssignmentRule failed, no variable with ID " + vid + " existed in the model");
 	}
 
-	if (sbmlModel->getRule(vid) != NULL || sbmlModel->getAssignmentRule(vid) != NULL)
+	if (sbmlModel->getRule(vid) != NULL)
 	{
-		throw std::invalid_argument("Roadrunner::addAssignmentRule failed, variable " + vid + " already has a rule or an assignment rule existing in the model");
+		throw std::invalid_argument("Roadrunner::addAssignmentRule failed, variable " + vid + " already has a rule existing in the model");
 	}
 
 	Log(Logger::LOG_DEBUG) << "Adding assignment rule for" << vid << "..." << endl;
@@ -5756,9 +5756,9 @@ void RoadRunner::addRateRule(const std::string& vid, const std::string& formula,
 		throw std::invalid_argument("Roadrunner::addRateRule failed, no variable with ID " + vid + " existed in the model");
 	}
 
-	if (sbmlModel->getRule(vid) != NULL || sbmlModel->getAssignmentRule(vid) != NULL)
+	if (sbmlModel->getRule(vid) != NULL)
 	{
-		throw std::invalid_argument("Roadrunner::addRateRule failed, variable " + vid + " already has a rule or an assignment rule existing in the model");
+		throw std::invalid_argument("Roadrunner::addRateRule failed, variable " + vid + " already has a rule existing in the model");
 	}
 
 
