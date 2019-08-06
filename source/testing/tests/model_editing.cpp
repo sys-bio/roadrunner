656d7e5ba587f13d41dc33628627ef38ad036a99
andrewbriand <atb8888@comcast.net>
2019-08-05 17:18:06 -0700

    Uncomment tests

--- source/testing/tests/model_editing.cpp
+++ source/testing/tests/model_editing.cpp
@@ -575,7 +575,7 @@ void removeAndReaddAllCompartments(RoadRunner *rri, libsbml::SBMLDocument *doc)
 
 SUITE(MODEL_EDITING_TEST_SUITE)
 {
-	/*TEST(ADD_REACTION_1)
+	TEST(ADD_REACTION_1)
 	{
 		CHECK(RunModelEditingTest([](RoadRunner* rri) {
 			rri->addReaction("reaction2", {"S2"}, {"S1"}, "k1*S2", true);
@@ -1164,7 +1164,7 @@ SUITE(MODEL_EDITING_TEST_SUITE)
 			rri->setPersistent("event1", true, false);
 			rri->setPersistent("event2", true);
 		}, "l3v1"));
-	}*/
+	}
 
 	TEST(SET_PERSISTENT_2)
 	{
@@ -1317,7 +1317,7 @@ SUITE(MODEL_EDITING_TEST_SUITE)
 		}, "l3v1"));
 	}
 
-	/*TEST(READD_SPECIES)
+	TEST(READD_SPECIES)
 	{
 		clog << endl << "==== CHECK_READD_SPECIES ====" << endl << endl;
 		for (int i = 1; i <= 38; i++)
@@ -1353,5 +1353,5 @@ SUITE(MODEL_EDITING_TEST_SUITE)
 				UnitTest::CurrentTest::Results()->OnTestFailure(*UnitTest::CurrentTest::Details(), failureMessage.c_str());
 			}
 		}
-	}*/
+	}
 }
