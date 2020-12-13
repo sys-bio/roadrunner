1dfc7972862867115e27c47a98092737b88705bc
Ciaran Welsh <cwelsh2@uw.edu>
2020-12-13 10:52:37 +0000

    azure-tests

--- azure-pipelines/get_llvm.sh
+++ azure-pipelines/get_llvm.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # use only in azure pipelines, where the variables that are needed are defined.
+# arguments:
+#   llvm_install_prefix: required.
+llvm_install_prefix=$1
 echo "current driectory is: $(pwd)"
 echo "ls $(ls)"
 wget -q https://github.com/sys-bio/llvm-6.x/releases/download/release%2F6.x/llvm-6.x-gcc7.5-x64-release.tar.gz
 tar -zxf llvm-6.x-gcc7.5-x64-release.tar.gz
-mv llvm-6.x-gcc7.5-x64-release/** '$(LLVM_INSTALL_PREFIX)'
-echo "cd to LLVM_INSTALL_PREFIX: $(LLVM_INSTALL_PREFIX)"
-cd $(LLVM_INSTALL_PREFIX)
+mv llvm-6.x-gcc7.5-x64-release/** '$llvm_install_prefix'
+echo "cd to LLVM_INSTALL_PREFIX: $llvm_install_prefix"
+cd $llvm_install_prefix
 echo "ls:"
 ls
