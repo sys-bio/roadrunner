600f5dde2d4747cd237a7d73937a682811b02eb0
Ciaran Welsh <cwelsh2@uw.edu>
2021-04-22 18:53:38 +0100

    Created a minimal working example for a picklable PySWIGObject which works.

---
+++ test/picklable-roadrunner/picklable_swig_tests.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+sys.path += [r"D:\roadrunner\roadrunner\install-msvc2019-rel\site-packages"]
+
+
+import roadrunner as rr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
