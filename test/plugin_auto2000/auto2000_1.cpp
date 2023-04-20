346e15cf865b0a926ff58557f6046848261a5fd8
adelhpour <>
2023-04-20 11:19:14 -0700

    uncommenting auto2000 plugin test #1

--- test/plugin_auto2000/auto2000_1.cpp
+++ test/plugin_auto2000/auto2000_1.cpp
@@ -14,9 +14,9 @@ TEST_F(PluginAuto2000Tests, Issue_773_no_boundary_species)
     //This just tests to make sure we don't crash if there's no boundary species.
     PluginManager* PM = new PluginManager(rrPluginsBuildDir_.string());
 
-    //Plugin* a2kplugin = PM->getPlugin("tel_auto2000");
-    //ASSERT_TRUE(a2kplugin != NULL);
-    /*
+    Plugin* a2kplugin = PM->getPlugin("tel_auto2000");
+    ASSERT_TRUE(a2kplugin != NULL);
+
     a2kplugin->setPropertyByString("SBML", (pluginsModelsDir / "auto2000_2rxn.xml").string().c_str());
     a2kplugin->setPropertyByString("PrincipalContinuationParameter", "k");
     a2kplugin->setPropertyByString("ScanDirection", "Positive");
@@ -24,5 +24,4 @@ TEST_F(PluginAuto2000Tests, Issue_773_no_boundary_species)
     a2kplugin->setPropertyByString("RL1", "5");
 
     a2kplugin->execute();
-     */
 }
