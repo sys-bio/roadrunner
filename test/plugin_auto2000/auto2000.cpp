8214cea1632039552031de9b8d3fe234d33f7cc1
adelhpour <>
2023-05-02 09:23:27 -0700

    back to ONE normal test for auto2000

---
+++ test/plugin_auto2000/auto2000.cpp
@@ -0,0 +1,42 @@
+#include "gtest/gtest.h"
+#include <filesystem>
+#include "RoadRunnerTest.h"
+#include "telPluginManager.h"
+#include "telPlugin.h"
+#include "telProperties.h"
+#include "telTelluriumData.h"
+#include "telProperty.h"
+#include "../../wrappers/C/telplugins_properties_api.h"
+
+using std::filesystem::path;
+
+using namespace testing;
+using namespace std;
+using namespace tlp;
+
+
+class PluginAuto2000Tests : public RoadRunnerTest {
+public:
+    path pluginsModelsDir;
+
+    PluginAuto2000Tests() {
+        pluginsModelsDir = rrTestModelsDir_ / "PLUGINS";
+    }
+};
+
+
+TEST_F(PluginAuto2000Tests, Issue_773_no_boundary_species)
+{
+    //This just tests to make sure we don't crash if there's no boundary species.
+    PluginManager* PM = new PluginManager(rrPluginsBuildDir_.string());
+
+    Plugin* a2kplugin = PM->getPlugin("tel_auto2000");
+    ASSERT_TRUE(a2kplugin != NULL);
+    a2kplugin->setPropertyByString("SBML", (pluginsModelsDir / "auto2000_2rxn.xml").string().c_str());
+    a2kplugin->setPropertyByString("PrincipalContinuationParameter", "k");
+    a2kplugin->setPropertyByString("ScanDirection", "Positive");
+    a2kplugin->setPropertyByString("RL0", "0");
+    a2kplugin->setPropertyByString("RL1", "5");
+
+    a2kplugin->execute();
+}
