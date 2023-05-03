ab3e77aad8debc3dfa3f390519e7d49ee0c4d8cd
adelhpour <>
2023-05-02 17:40:27 -0700

    add another test inside the previous one to avoid using plugin manger more than once

--- test/plugin_auto2000/auto2000.cpp
+++ test/plugin_auto2000/auto2000.cpp
@@ -24,11 +24,13 @@ public:
     }
 };
 
-//This just tests to make sure we don't crash if there's no boundary species.
-PluginManager* PM = new PluginManager(rrPluginsBuildDir_.string());
 
 TEST_F(PluginAuto2000Tests, Issue_773_no_boundary_species)
 {
+    //This just tests to make sure we don't crash if there's no boundary species.
+    PluginManager* PM = new PluginManager(rrPluginsBuildDir_.string());
+
+    // test #1
     Plugin* a2kplugin = PM->getPlugin("tel_auto2000");
     ASSERT_TRUE(a2kplugin != NULL);
     a2kplugin->setPropertyByString("SBML", (pluginsModelsDir / "auto2000_2rxn.xml").string().c_str());
@@ -38,6 +40,46 @@ TEST_F(PluginAuto2000Tests, Issue_773_no_boundary_species)
     a2kplugin->setPropertyByString("RL1", "5");
 
     a2kplugin->execute();
-}
 
-delete PM;
+
+    // test #2
+    a2kplugin->setPropertyByString("SBML", (pluginsModelsDir / "BIOMD0000000203.xml").string().c_str());
+    a2kplugin->setPropertyByString("ScanDirection", "Positive");
+    a2kplugin->setPropertyByString("PrincipalContinuationParameter", "A");
+    a2kplugin->setPropertyByString("PCPLowerBound", "10");
+    a2kplugin->setPropertyByString("PCPUpperBound", "200");
+    a2kplugin->setPropertyByString("NMX", "5000");
+
+    a2kplugin->execute();
+
+    string summary = a2kplugin->getPropertyValueAsString("BifurcationSummary");
+    string headers = "BR    PT  TY LAB    PAR(0)        L2-NORM         U(1)          U(2)          U(3)          U(4)          U(5)";
+    EXPECT_EQ(summary.find(headers), 4);
+
+    vector<int>* points = (vector<int>*)a2kplugin->getPropertyValueHandle("BifurcationPoints");
+    ASSERT_TRUE(points != NULL);
+    ASSERT_EQ(points->size(), 4);
+    EXPECT_EQ(points->at(0), 1);
+    EXPECT_EQ(points->at(1), 1385);
+    EXPECT_EQ(points->at(2), 2189);
+    EXPECT_EQ(points->at(3), 3705);
+
+    StringList* labels = (StringList*)a2kplugin->getPropertyValueHandle("BifurcationLabels");
+    ASSERT_TRUE(labels != NULL);
+    ASSERT_EQ(labels->size(), 4);
+    EXPECT_EQ(labels->asString(), "EP,LP,LP,EP");
+
+    TelluriumData* data = (TelluriumData*)a2kplugin->getPropertyValueHandle("BifurcationData");
+    ASSERT_TRUE(data != NULL);
+    EXPECT_EQ(data->cSize(), 6);
+    EXPECT_EQ(data->rSize(), 3705);
+    EXPECT_EQ(data->getColumnNamesAsString(), "A,[OCT4],[SOX2],[NANOG],[Protein],[OCT4_SOX2]");
+    //Spot checks:
+    EXPECT_NEAR(data->getDataElement(6, 0), 10.036, 0.001);
+    EXPECT_NEAR(data->getDataElement(52, 1), 8.85978, 0.0001);
+    EXPECT_NEAR(data->getDataElement(167, 2), 12.3176, 0.0001);
+    EXPECT_NEAR(data->getDataElement(623, 3), 0.0469995, 0.0001);
+    EXPECT_NEAR(data->getDataElement(1522, 4), 33.0702, 0.0001);
+    EXPECT_NEAR(data->getDataElement(2345, 5), 22.6297, 0.0001);
+    EXPECT_NEAR(data->getDataElement(3535, 6), 183.378, 0.001);
+}
