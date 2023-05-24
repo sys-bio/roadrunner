be056be704500ab0fa18151211b80dbee054fc77
adelhpour <>
2023-05-23 18:06:38 -0700

    one test from lm, mc, and nm plugins are added

--- test/plugin_auto2000/PluginTests.cpp
+++ test/plugin_auto2000/PluginTests.cpp
@@ -6,12 +6,12 @@
 #include "telProperties.h"
 #include "telTelluriumData.h"
 #include "telProperty.h"
-#include "../../wrappers/C/telplugins_properties_api.h"
+//#include "../../wrappers/C/telplugins_properties_api.h"
 
 using std::filesystem::path;
 
-using namespace testing;
-using namespace std;
+//using namespace testing;
+//using namespace std;
 using namespace tlp;
 
 
@@ -413,3 +413,303 @@ TEST_F(PluginTests, TEST_MODEL_NEW_MODEL)
     EXPECT_EQ(sim->rSize(), 14);
 }
  */
+
+TEST_F(PluginTests, LEVENBERG_MARQUARDT_OPTIMIZE_TEST_MODEL)
+{
+    Plugin* tmplugin = PM->getPlugin("tel_test_model");
+    ASSERT_TRUE(tmplugin != NULL);
+
+    // reset the value of plugin properties
+    tmplugin->resetPropertiesValues();
+
+    tmplugin->execute();
+
+    Plugin* lmplugin = PM->getPlugin("tel_levenberg_marquardt");
+    ASSERT_TRUE(lmplugin != NULL);
+
+    // reset the value of plugin properties
+    lmplugin->resetPropertiesValues();
+
+    Plugin* chiplugin = PM->getPlugin("tel_chisquare");
+    ASSERT_TRUE(chiplugin != NULL);
+
+    // reset the value of plugin properties
+    chiplugin->resetPropertiesValues();
+
+    PropertyBase* sbml = tmplugin->getProperty("Model");
+    lmplugin->setPropertyByString("SBML", sbml->getValueAsString().c_str());
+
+    PropertyBase* testdata = tmplugin->getProperty("TestDataWithNoise");
+    TelluriumData* exdata = static_cast<TelluriumData*>(testdata->getValueHandle());
+    lmplugin->setPropertyValue("ExperimentalData", exdata);
+
+    Property<double> k1val(0.3, "k1", "", "", "", true);
+    Properties ipl;
+    ipl.add(&k1val);
+    //tlp::Property tpcre();
+    lmplugin->setPropertyValue("InputParameterList", &ipl);
+    lmplugin->setPropertyByString("FittedDataSelectionList", "[S1] [S2]");
+    lmplugin->setPropertyByString("ExperimentalDataSelectionList", "[S1] [S2]");
+
+    lmplugin->execute();
+    EXPECT_EQ(lmplugin->getPropertyValueAsString("StatusMessage").find("converged"), 0);
+
+    PropertyBase* hessian_property = lmplugin->getProperty("Hessian");
+    ASSERT_TRUE(hessian_property != NULL);
+    TelluriumData* hessian = static_cast<TelluriumData*>(hessian_property->getValueHandle());
+    EXPECT_EQ(hessian->rSize(), 1);
+    EXPECT_EQ(hessian->cSize(), 1);
+    EXPECT_NEAR(hessian->getDataElement(0, 0), 3300, 1500); //Determined empirically.
+
+    PropertyBase* cov_property = lmplugin->getProperty("CovarianceMatrix");
+    ASSERT_TRUE(cov_property != NULL);
+    TelluriumData* covariance = static_cast<TelluriumData*>(cov_property->getValueHandle());
+    EXPECT_EQ(covariance->rSize(), 1);
+    EXPECT_EQ(covariance->cSize(), 1);
+    EXPECT_NEAR(covariance->getDataElement(0, 0), 0.0003, 0.0001); //Determined empirically.
+
+    PropertyBase* chi_property = lmplugin->getProperty("ChiSquare");
+    ASSERT_TRUE(chi_property != NULL);
+    double* chisquare = static_cast<double*>(chi_property->getValueHandle());
+    EXPECT_NEAR(*chisquare, 76, 70); //Determined empirically.
+
+    PropertyBase* red_chi_property = lmplugin->getProperty("ReducedChiSquare");
+    ASSERT_TRUE(red_chi_property != NULL);
+    double* reduced_chi = static_cast<double*>(red_chi_property->getValueHandle());
+    EXPECT_NEAR(*reduced_chi, 1.0, 0.8); //Determined empirically.
+
+    PropertyBase* outparam_property = lmplugin->getProperty("OutputParameterList");
+    ASSERT_TRUE(outparam_property != NULL);
+    Properties* outparams = static_cast<Properties*>(outparam_property->getValueHandle());
+    PropertyBase* outparam = outparams->getFirst();
+    ASSERT_TRUE(outparam != NULL);
+    EXPECT_EQ(outparam->getName(), "k1");
+    double* outparam_val = static_cast<double*>(outparam->getValueHandle());
+    EXPECT_NEAR(*outparam_val, 1.0, 0.5);
+    EXPECT_TRUE(outparams->getNext() == NULL);
+
+    PropertyBase* conflimit_property = lmplugin->getProperty("ConfidenceLimits");
+    ASSERT_TRUE(conflimit_property != NULL);
+    Properties* conflimits = static_cast<Properties*>(conflimit_property->getValueHandle());
+    PropertyBase* conflimit = conflimits->getFirst();
+    ASSERT_TRUE(conflimit != NULL);
+    EXPECT_EQ(conflimit->getName(), "k1_confidence");
+    double* conflimit_val = static_cast<double*>(conflimit->getValueHandle());
+    EXPECT_NEAR(*conflimit_val, 0.03, 0.02);
+    EXPECT_TRUE(conflimits->getNext() == NULL);
+
+    PropertyBase* fit_property = lmplugin->getProperty("FittedData");
+    ASSERT_TRUE(fit_property != NULL);
+    TelluriumData* fit = static_cast<TelluriumData*>(fit_property->getValueHandle());
+    EXPECT_EQ(fit->cSize(), 3);
+    EXPECT_EQ(fit->rSize(), 14);
+
+    PropertyBase* residuals_property = lmplugin->getProperty("Residuals");
+    ASSERT_TRUE(residuals_property != NULL);
+    TelluriumData* residuals = static_cast<TelluriumData*>(residuals_property->getValueHandle());
+    EXPECT_EQ(residuals->cSize(), 3);
+    EXPECT_EQ(residuals->rSize(), 14);
+
+    for (int c = 0; c < fit->cSize(); c++)
+    {
+        for (int r = 0; r < fit->rSize(); r++)
+        {
+        double fitval = fit->getDataElement(r, c);
+        double origval = exdata->getDataElement(r, c);
+        double tol = fmax(abs(origval / 10), 0.0001);
+        EXPECT_NEAR(fitval, origval, tol);
+
+            if (c > 0) {
+                double residual = residuals->getDataElement(r, c);
+                EXPECT_NEAR(abs(origval - fitval), residual, 0.0001);
+                EXPECT_LT(residual, 0.0001);
+                //cout << origval << ", " << fitval << ", " << residual << ", " << abs(origval - fitval) << endl;
+            }
+        }
+    }
+}
+
+TEST_F(PluginTests, MONTE_CARLO_OPTIMIZE_TEST_MODEL)
+{
+    Plugin* tmplugin = PM->getPlugin("tel_test_model");
+    ASSERT_TRUE(tmplugin != NULL);
+
+    // reset the value of plugin properties
+    tmplugin->resetPropertiesValues();
+
+    tmplugin->execute();
+
+    Plugin* mcplugin = PM->getPlugin("tel_monte_carlo_bs");
+    ASSERT_TRUE(mcplugin != NULL);
+
+    // reset the value of plugin properties
+    mcplugin->resetPropertiesValues();
+
+    PropertyBase* sbml = tmplugin->getProperty("Model");
+    mcplugin->setPropertyByString("SBML", sbml->getValueAsString().c_str());
+    PropertyBase* testdata = tmplugin->getProperty("TestDataWithNoise");
+    TelluriumData* exdata = static_cast<TelluriumData*>(testdata->getValueHandle());
+    mcplugin->setPropertyValue("ExperimentalData", exdata);
+
+    Property<double> k1val(0.3, "k1", "", "", "", true);
+    Properties ipl;
+    ipl.add(&k1val);
+    //tlp::Property tpcre();
+    mcplugin->setPropertyValue("InputParameterList", &ipl);
+    mcplugin->setPropertyByString("NrOfMCRuns", "100");
+    mcplugin->setPropertyByString("FittedDataSelectionList", "[S1] [S2]");
+    mcplugin->setPropertyByString("ExperimentalDataSelectionList", "[S1] [S2]");
+
+    mcplugin->execute();
+    //EXPECT_EQ(mcplugin->getPropertyValueAsString("StatusMessage").find("converged"), 0);
+
+    TelluriumData* params = static_cast<TelluriumData*>(mcplugin->getPropertyValueHandle("MonteCarloParameters"));
+    ASSERT_TRUE(params != NULL);
+    EXPECT_EQ(params->rSize(), 100);
+    EXPECT_EQ(params->cSize(), 1);
+    for (int r = 0; r < params->rSize(); r++)
+    {
+        EXPECT_NEAR(params->getDataElement(r, 0), 1.0, 0.2);
+    }
+
+    Properties* means = static_cast<Properties*>(mcplugin->getPropertyValueHandle("Means"));
+    ASSERT_TRUE(means != NULL);
+    Property<double>* mean1 = static_cast<Property<double>*>(means->getFirst());
+    EXPECT_EQ(mean1->getName(), "k1");
+    EXPECT_NEAR(mean1->getValue(), 1.0, 0.1);
+
+    Properties* conf_intervals = static_cast<Properties*>(mcplugin->getPropertyValueHandle("ConfidenceIntervals"));
+    ASSERT_TRUE(conf_intervals != NULL);
+    Property<double>* ci = static_cast<Property<double>* >(conf_intervals->getFirst());
+    EXPECT_EQ(ci->getName(), "k1");
+    EXPECT_NEAR(ci->getValue(), 0.05, 0.03);
+
+    Properties* percentiles = static_cast<Properties*>(mcplugin->getPropertyValueHandle("Percentiles"));
+    ASSERT_TRUE(percentiles != NULL);
+    Property<double>* percentile = static_cast<Property<double>*>(percentiles->getFirst());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_25_percentile");
+    EXPECT_NEAR(percentile->getValue(), 0.99, 0.12);
+
+    percentile = static_cast<Property<double>*>(percentiles->getNext());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_75_percentile");
+    EXPECT_NEAR(percentile->getValue(), 1.01, 0.12);
+
+    percentile = static_cast<Property<double>*>(percentiles->getNext());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_02.5_percentile");
+    EXPECT_NEAR(percentile->getValue(), 0.96, 0.13);
+
+    percentile = static_cast<Property<double>*>(percentiles->getNext());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_97.5_percentile");
+    EXPECT_NEAR(percentile->getValue(), 1.04, 0.13);
+
+    percentile = static_cast<Property<double>*>(percentiles->getNext());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_01_percentile");
+    EXPECT_NEAR(percentile->getValue(), 0.95, 0.14);
+
+    percentile = static_cast<Property<double>*>(percentiles->getNext());
+    ASSERT_TRUE(percentile != NULL);
+    EXPECT_EQ(percentile->getName(), "k1_99_percentile");
+    EXPECT_NEAR(percentile->getValue(), 1.05, 0.14);
+
+    EXPECT_TRUE(percentiles->getNext() == NULL);
+}
+
+TEST_F(PluginTests, NELDER_MEAD_OPTIMIZE_TEST_MODEL)
+{
+    Plugin* tmplugin = PM->getPlugin("tel_test_model");
+    ASSERT_TRUE(tmplugin != NULL);
+
+    // reset the value of plugin properties
+    tmplugin->resetPropertiesValues();
+
+    tmplugin->execute();
+
+    Plugin* nmplugin = PM->getPlugin("tel_nelder_mead");
+    ASSERT_TRUE(nmplugin != NULL);
+
+    // reset the value of plugin properties
+    nmplugin->resetPropertiesValues();
+
+    PropertyBase* sbml = tmplugin->getProperty("Model");
+    nmplugin->setPropertyByString("SBML", sbml->getValueAsString().c_str());
+
+    PropertyBase* testdata = tmplugin->getProperty("TestDataWithNoise");
+    TelluriumData* exdata = static_cast<TelluriumData*>(testdata->getValueHandle());
+    nmplugin->setPropertyValue("ExperimentalData", exdata);
+
+    Property<double> k1val(0.3, "k1", "", "", "", true);
+    Properties ipl;
+    ipl.add(&k1val);
+    //tlp::Property tpcre();
+    nmplugin->setPropertyValue("InputParameterList", &ipl);
+    nmplugin->setPropertyByString("FittedDataSelectionList", "[S1] [S2]");
+    nmplugin->setPropertyByString("ExperimentalDataSelectionList", "[S1] [S2]");
+
+    nmplugin->execute();
+    //The NM plugin has no status message.
+    //EXPECT_EQ(nmplugin->getPropertyValueAsString("StatusMessage").find("converged"), 0);
+
+    PropertyBase* hessian_property = nmplugin->getProperty("Hessian");
+    ASSERT_TRUE(hessian_property == NULL);
+
+    PropertyBase* cov_property = nmplugin->getProperty("CovarianceMatrix");
+    ASSERT_TRUE(cov_property == NULL);
+
+    PropertyBase* chi_property = nmplugin->getProperty("ChiSquare");
+    ASSERT_TRUE(chi_property != NULL);
+    double* chisquare = static_cast<double*>(chi_property->getValueHandle());
+    EXPECT_NEAR(*chisquare, 76, 70); //Determined empirically.
+
+    PropertyBase* red_chi_property = nmplugin->getProperty("ReducedChiSquare");
+    ASSERT_TRUE(red_chi_property != NULL);
+    double* reduced_chi = static_cast<double*>(red_chi_property->getValueHandle());
+    EXPECT_NEAR(*reduced_chi, 2.8, 2.4); //Determined empirically.
+
+    PropertyBase* outparam_property = nmplugin->getProperty("OutputParameterList");
+    ASSERT_TRUE(outparam_property != NULL);
+    Properties* outparams = static_cast<Properties*>(outparam_property->getValueHandle());
+    PropertyBase* outparam = outparams->getFirst();
+    ASSERT_TRUE(outparam != NULL);
+    EXPECT_EQ(outparam->getName(), "k1");
+    double* outparam_val = static_cast<double*>(outparam->getValueHandle());
+    EXPECT_NEAR(*outparam_val, 0.925, 0.2);
+    EXPECT_TRUE(outparams->getNext() == NULL);
+
+    PropertyBase* conflimit_property = nmplugin->getProperty("ConfidenceLimits");
+    ASSERT_TRUE(conflimit_property == NULL);
+
+    PropertyBase* fit_property = nmplugin->getProperty("FittedData");
+    ASSERT_TRUE(fit_property != NULL);
+    TelluriumData* fit = static_cast<TelluriumData*>(fit_property->getValueHandle());
+    EXPECT_EQ(fit->cSize(), 3);
+    EXPECT_EQ(fit->rSize(), 14);
+
+    PropertyBase* residuals_property = nmplugin->getProperty("Residuals");
+    ASSERT_TRUE(residuals_property != NULL);
+    TelluriumData* residuals = static_cast<TelluriumData*>(residuals_property->getValueHandle());
+    EXPECT_EQ(residuals->cSize(), 3);
+    EXPECT_EQ(residuals->rSize(), 14);
+
+    for (int c = 0; c < fit->cSize(); c++)
+    {
+        for (int r = 0; r < fit->rSize(); r++)
+        {
+            double fitval = fit->getDataElement(r, c);
+            double origval = exdata->getDataElement(r, c);
+            double tol = fmax(abs(origval / 10), 0.0001);
+            EXPECT_NEAR(fitval, origval, tol);
+
+            if (c > 0) {
+                double residual = abs(residuals->getDataElement(r, c));
+                EXPECT_NEAR(abs(origval - fitval), residual, 0.0002);
+                EXPECT_LT(residual, 2.2e-5);
+                //cout << origval << ", " << fitval << ", " << residual << ", " << abs(origval - fitval) << endl;
+            }
+        }
+    }
+}
