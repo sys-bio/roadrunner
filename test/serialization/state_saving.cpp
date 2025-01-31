91a79dc88f4e94d3b26df10c7863c51332a7ebc3
Lucian Smith <lpsmith@uw.edu>
2025-01-30 21:58:47 -0800

    Re-enable 'load valid file' test.
    Need to get new save state files every time we update the minor version number of roadrunner.  This (hopefully) puts in a linux version, meaning that only the macs will fail.

--- test/serialization/state_saving.cpp
+++ test/serialization/state_saving.cpp
@@ -434,7 +434,7 @@ TEST_F(StateSavingTests, LOAD_INVALID_FILE) {
     EXPECT_THROW(rri.loadState((stateSavingModelsDir / "wrong-save-state.rr").string()), std::exception);
 }
 
-TEST_F(StateSavingTests, DISABLED_LOAD_VALID_FILE) {
+TEST_F(StateSavingTests, LOAD_VALID_FILE) {
     //NOTE:  This test will fail every time roadrunner comes out with a new minor
     // version, and/or the save state format changes.  In fact, if the save state
     // format changes, the minor version *must* change, so that existing roadrunner
