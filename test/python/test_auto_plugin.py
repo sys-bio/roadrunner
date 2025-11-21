78cab347b3062fe44cc76fa5ad8b39eefb172aed
Lucian Smith <lpsmith@uw.edu>
2025-11-21 12:44:57 -0800

    Test the plugins in python.
    Issue #1259 is odd--the issue seems to be roadrunner calling 'load' from inside a plugin, which shouldn't normally be a problem.  Test loading and running auto to see if we still get the problem on natively-compiled computational environments.

---
+++ test/python/test_auto_plugin.py
@@ -0,0 +1,36 @@
+import unittest
+import roadrunner
+import rrplugins
+
+print(roadrunner.__file__)
+
+try:
+    from roadrunner.tests import TestModelFactory as tmf
+except:
+    import TestModelFactory as tmf
+
+class AutoPluginTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def loadTestModel(self, modelName: str):
+        """Instantiate instance of TestModel called modelName"""
+        self.checkValidTestModelName(modelName)
+        testModel = tmf.TestModelFactory(modelName)
+        self.checkTestModelImplements(testModel, tmf.StructuralProperties)
+        return testModel
+
+    def test_running_auto(self):
+        testModel = self.loadTestModel("BimolecularEnd")
+        auto = rrplugins.Plugin("tel_auto2000")
+        auto.setProperty("SBML", testModel.str())
+        auto.execute()
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
